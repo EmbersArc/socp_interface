abce42235777b09cefefb76766f7e110d84746b6
Sven Niederberger <svenn@ethz.ch>
2019-10-09 12:16:16 +0200

    use typedef

--- src/ecosWrapper.cpp
+++ src/ecosWrapper.cpp
@@ -11,7 +11,7 @@ using std::pair;
 using std::tuple;
 using std::vector;
 
-static_assert(std::is_same_v<idxint, long>,
+static_assert(std::is_same_v<idxint, SuiteSparse_long>,
               "Definitions of idxint might not be consistent."
               "Make sure ECOS is compiled with USE_LONG = 1.");
 
