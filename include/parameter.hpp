66c7124ddf65eb756065e755cbd001d457d3f1f1
Sven Niederberger <s-niederberger@hotmail.com>
2019-12-18 14:21:44 +0100

    more flexible parameters

--- include/parameter.hpp
+++ include/parameter.hpp
@@ -2,6 +2,7 @@
 
 #include <functional>
 #include <string>
+#include <utility>
 #include <variant>
 
 namespace op
@@ -11,32 +12,49 @@ enum class ParameterSourceType
 {
     Constant,
     Pointer,
-    Callback
+    Callback,
+    Operation
 };
+
+struct ParameterOperation;
 
 // Represents a parameter p_i in the opt-problem that can be changed between problem evaluations.
 // The parameter value can either be constant or dynamically accessed through a pointer or callback.
 class Parameter
 {
-    std::variant<std::function<double()>,
-                 const double *,
-                 double>
-        source;
-
+    using parameter_variant_t = std::variant<double,
+                                             const double *,
+                                             std::function<double()>,
+                                             ParameterOperation *>;
+    parameter_variant_t source;
     ParameterSourceType sourceType;
 
 public:
     Parameter();
-
-    explicit Parameter(std::function<double()> callback);
-
+    Parameter(double const_value);
     explicit Parameter(const double *dynamic_value_ptr);
-
-    explicit Parameter(double const_value);
+    explicit Parameter(std::function<double()> callback);
+    explicit Parameter(ParameterOperation *operation);
+    ~Parameter();
 
     double get_value() const;
 
-    friend std::ostream& operator<<(std::ostream& os, const Parameter& parameter);
+    friend std::ostream &operator<<(std::ostream &os, const Parameter &parameter);
+
+    friend Parameter operator+(const Parameter &lhs, const Parameter &rhs);
+    friend Parameter operator-(const Parameter &lhs, const Parameter &rhs);
+    friend Parameter operator-(const Parameter &par);
+    friend Parameter operator*(const Parameter &lhs, const Parameter &rhs);
+    friend Parameter operator/(const Parameter &lhs, const Parameter &rhs);
+};
+
+struct ParameterOperation
+{
+    ParameterOperation(const Parameter &lhs, const Parameter &rhs,
+                       const std::function<double(double, double)> &operation);
+    std::pair<const Parameter, const Parameter> terms;
+    std::function<double(double, double)> operation;
+    double get_value() const;
 };
 
 } // namespace op
