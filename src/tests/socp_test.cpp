cd3ffc00ae8304b31eafa808e1da9cae5f297cbb
Sven Niederberger <s-niederberger@outlook.com>
2020-01-20 18:50:45 +0100

    add comments

--- src/tests/socp_test.cpp
+++ src/tests/socp_test.cpp
@@ -11,9 +11,7 @@
 
 int main()
 {
-    auto t1 = std::chrono::high_resolution_clock::now();
-
-    // Set up problem data
+    // Set up problem data.
 
     // number of second order cone constraints
     const size_t m = 3;
@@ -46,36 +44,61 @@ int main()
     F.setRandom();
     Eigen::Matrix<double, p, 1> g = F * x0;
 
-    // Formulate SOCP
+    // Formulate SOCP.
+    auto t0 = std::chrono::high_resolution_clock::now();
+
+    // Create the SOCP instance.
     op::SecondOrderConeProgram socp;
 
+    // Add variables. Those can be scalars, vectors or matrices.
     op::Variable x = socp.createVariable("x", n);
 
+    // Add constraints.
     for (size_t i = 0; i < m; i++)
     {
         socp.addConstraint(op::Norm2(op::Parameter(A[i]) * x + op::Parameter(b[i])) <=
                            op::Parameter(c[i]).transpose() * x + op::Parameter(d[i]));
     }
     socp.addConstraint(op::Parameter(F) * x == op::Parameter(g));
-    socp.addMinimizationTerm(op::Parameter(f).transpose() * x);
+
+    // Here we use a pointer to a parameter. This allows changing it dynamically.
+    socp.addMinimizationTerm(op::Parameter(&f).transpose() * x);
+
+    // Print the problem for inspection.
     std::cout << socp << "\n\n";
 
-    // Solve SOCP
+    // Create the solver instance.
     op::Solver solver(socp);
 
+    auto t = std::chrono::high_resolution_clock::now();
+    auto t_setup = std::chrono::duration_cast<std::chrono::microseconds>(t - t0).count();
+    std::cout << "\nSetup duration: " << t_setup << "μs.\n\n";
+
+    // Solve the problem and show solver output.
+    t0 = std::chrono::high_resolution_clock::now();
     solver.solveProblem(true);
 
+    // Check if the solver has produced a valid solution.
     assert(socp.isFeasible());
 
-    // Get Solution
+    t = std::chrono::high_resolution_clock::now();
+    auto t_solve = std::chrono::duration_cast<std::chrono::microseconds>(t - t0).count();
+    std::cout << "\nSolver duration: " << t_solve << "μs.\n\n";
+
+    // Get Solution.
     Eigen::Matrix<double, n, 1> x_sol;
     socp.readSolution("x", x_sol);
 
-    std::cout << "Solution:\n"
+    // Print the first solution.
+    std::cout << "Solution 1:\n"
               << x_sol << "\n\n";
 
-    auto t2 = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+    // Change the problem parameters and solve again.
+    f.setRandom();
+    solver.solveProblem(false);
+    socp.readSolution("x", x_sol);
 
-    std::cout << "All tests were successful (" << duration << "μs).\n";
+    // Print the new solution.
+    std::cout << "Solution 2:\n"
+              << x_sol << "\n\n";
 }
